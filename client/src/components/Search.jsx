dc8751fecb73c2329f17a0b27c8465663ed69370
Henry Yang <contacthenryyang@gmail.com>
2018-02-18 13:44:30 -0800

    Remove buggy enter to search

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -13,6 +13,15 @@ class Search extends React.Component {
       term: e.target.value
     });
   }
+/*
+  enterToSubmit(event) {
+    if (event.key === 'Enter') {
+      console.log('Enter key pressed');
+      console.log('this = ', this)
+      this.search()
+      this.props.onSearch(this.state.term);
+    }
+  }*/
 
   search() {
     this.props.onSearch(this.state.term);
@@ -21,7 +30,7 @@ class Search extends React.Component {
   render() {
     return (<div>
       <h4>Add more repos!</h4>
-      Enter a github username: <input value={this.state.terms} onChange={this.onChange.bind(this)} onKeyDown={this.search.bind(this)}/>       
+      Enter a github username: <input value={this.state.terms} onChange={this.onChange.bind(this)} /*onKeyPress={this.enterToSubmit}*//>       
       <button onClick={this.search.bind(this)}> Add Repos </button>
     </div>) 
   }
