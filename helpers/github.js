ecc8257ed1f98565b5e4387cfd3cfd4fc2b71e77
Henry Yang <contacthenryyang@gmail.com>
2018-02-17 21:35:50 -0800

    Server can post request to github and get response
    Need to debug- server crashes after receiving body

--- helpers/github.js
+++ helpers/github.js
@@ -1,20 +1,27 @@
 const request = require('request');
 const config = require('../config.js');
 
-let getReposByUsername = (/* TODO */) => {
+let getReposByUsername = (searchTerm, callback) => {
   // TODO - Use the request module to request repos for a specific
   // user from the github API
 
   // The options object has been provided to help you out, 
   // but you'll have to fill in the URL
   let options = {
-    url: 'FILL ME IN',
+    url: `https://api.github.com/users/${searchTerm}`,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
-
+  request(options, (error, response, body) => {
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+    console.log('body:', body);
+    console.log('typeof body =', typeof body)
+    console.log('jsonparsed body = ', JSON.parse(body));
+    callback(body);
+  })
 }
 
 module.exports.getReposByUsername = getReposByUsername;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 let app = express();
 const bodyParser = require('body-parser');
+const github = require('../helpers/github.js');
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.text())
@@ -11,6 +12,10 @@ app.post('/repos', function (req, res) {
   // and get the repo information from the github API, then
   // save the repo information in the database
   console.log('req.body = ', req.body)
+  var searchTerm = req.body;
+  // call the github helper, pass in res.send as a callback
+  github.getReposByUsername(searchTerm, res.send) // Data sent back to client
+  res.send('Server received request!'); 
 });
 
 app.get('/repos', function (req, res) {
