128c1b36899e41c7060a90631eb2cbcda5e36b0d
Henry Yang <contacthenryyang@gmail.com>
2018-02-17 23:25:44 -0800

    Frame out beginnings, but not more functional

--- database/index.js
+++ database/index.js
@@ -3,14 +3,37 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  stargazers_count: Number,
+  full_name: {type: String, unique: true,},
+  avatar_url: String,
+  html_url: String,
+  forks: Number,
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (arrInput) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  console.log('in save now');
+  arrInput.forEach((element) => {
+    var currentItem = new Repo({
+                                stargazers_count: element.stargazers_count,
+                                full_name: element.full_name,
+                                avatar_url: element.avatar_url,
+                                html_url: element.html_url,
+                                forks: element.forks,
+                              });
+    console.log('currentItem = ', currentItem)
+    currentItem.save((err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('Doc saved!!!');
+      }
+    })
+  })
 }
 
 module.exports.save = save;
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 let app = express();
 const bodyParser = require('body-parser');
 const github = require('../helpers/github.js');
+const mongoHelp = require('../database/index.js')
+
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.text())
@@ -14,8 +16,11 @@ app.post('/repos', function (req, res) {
   console.log('req.body = ', req.body)
   var searchTerm = req.body;
   // call the github helper
-  github.getReposByUsername(searchTerm, () => { 
-    console.log('back from api to server');
+  github.getReposByUsername(searchTerm, (err, data) => { 
+    // console.log('back from api to server');
+    console.error('err = ', err)
+    console.log('data = ', data)
+    mongoHelp.save;
     // Pass in save to mongo as a callback
       // pass in res.send as a callback to that
        // res.send()
